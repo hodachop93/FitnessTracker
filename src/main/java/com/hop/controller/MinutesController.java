815cb0a08d5c7fac4132bcd943e4eecd7f936df8
hodachop93 <hodachop93@gmail.com>
2016-07-08 20:30:36 +0700

    Resolving static files

--- src/main/java/com/hop/controller/MinutesController.java
+++ src/main/java/com/hop/controller/MinutesController.java
@@ -11,6 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MinutesController {
 
+    /*@RequestMapping(value = "/addMinutes")
+    public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
+        System.out.println("exercise: " + exercise.getMinutes());
+        return "redirect:addMoreMinutes";
+    }*/
+
+    @RequestMapping(value = "/addMoreMinutes")
+    public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+        System.out.println("exercising: " + exercise.getMinutes());
+        return "addMinutes";
+    }
+
     @RequestMapping(value = "/addMinutes")
     public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
         System.out.println("exercise: " + exercise.getMinutes());
